8abf62fa133c65ec5355c2761a814610db1cf8ad
slenane <slenane1@gmail.com>
2021-12-23 14:39:36 +0000

    Section 5: Rendering Lists & Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,43 +1,28 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from './ExpensesChart';
 import Card from '../UI/Card';
 
 import './Expenses.css';
 
 const Expenses = (props) => {
-     const [selectedYear, setSelectedYear] = useState('2021');
-
+    const [selectedYear, setSelectedYear] = useState('2021');
+    
+    
     const changeYearFilterHandler = (filteredYear) => {
         setSelectedYear(filteredYear);
-        console.log(filteredYear);
     };
+    
+    const filteredExpenses = props.items.filter(expense => (expense.date.getFullYear()).toString() === selectedYear);
 
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={selectedYear} onChangeYearFilter={changeYearFilterHandler} />    
-                <ExpenseItem
-                    title={props.data[0].title}
-                    amount={props.data[0].amount}
-                    date={props.data[0].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.data[1].title}
-                    amount={props.data[1].amount}
-                    date={props.data[1].date}
-                ></ExpenseItem>
-                <ExpenseItem 
-                    title={props.data[2].title}
-                    amount={props.data[2].amount}
-                    date={props.data[2].date}
-                ></ExpenseItem>
-                <ExpenseItem 
-                    title={props.data[3].title}
-                    amount={props.data[3].amount}
-                    date={props.data[3].date}
-                ></ExpenseItem>
+                <ExpensesFilter selected={selectedYear} onChangeYearFilter={changeYearFilterHandler} />   
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
 
